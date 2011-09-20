04adee753ceadcbef0284b1baddbeb4e172b7102
notyy <notyycn@gmail.com>
2011-09-20 15:15:10 +0800

    refactor

--- src/main/scala/com/kaopua/recall/Interpreter.scala
+++ src/main/scala/com/kaopua/recall/Interpreter.scala
@@ -32,20 +32,12 @@ input :h for list of comands
 
   def explain(userInput: String): Command = {
     logger.info("explaining userInput: " + userInput)
-    val SubContentPattern = """_(\d+)""".r
-    val SubMarkPattern = """_(\d+)=(.*)""".r
+    val MarkPattern = """(.*)=(.*)""".r
     userInput match {
       case ":h" => Help()
       case ":q" => Quit()
       case "_" => LastMemory()
       case s if s.startsWith("_+") => Accumulate(s.stripPrefix("_+"))
-      case SubContentPattern(i) => SubContent(lastMemory, i.toInt)
-      case SubMarkPattern(i, content) => {
-        lastMemory.getSubContent(i.toInt) match {
-          case Some(subHint: String) => SubMark(subHint, content)
-          case None => Error("subcontent not found by index " + i)
-        }
-      }
       case "" => {
         if (indents.size > 1) {
           indents = indents.tail
@@ -54,33 +46,30 @@ input :h for list of comands
           EndMark(markStack)
         }
       }
-      case _ =>
-        if (userInput.contains("=")) {
-          val markArray = userInput.split("=")
-          val hint = markArray(0)
-          val content = markArray(1)
-          logger.debug("processing hint {},content {}", hint, content)
-          if (content == "None") Remove(hint)
-          else if (hint.contains(".")) {
-            logger.debug("hint contains '.'")
-            var indent = hint.substring(0, hint.lastIndexOf("."))
-            if (indents.size > 0) {
-              markStack = Mark(indents.head + "." + hint, content) :: markStack
-              indent = indents.head + "." + indent
-              indents = indent :: indents
-            } else {
-              markStack = Mark(hint, content) :: markStack
-              indents = indent :: indents
-            }
-            Continue(" " * indent.length())
-          } else if (indents.size > 0) {
-            markStack = Mark(indents.head + "." + hint, content) :: markStack
-            Continue(" " * indents.head.length())
-          } else Mark(hint, content)
-        } else { //should be some kind of recall
-          if (userInput.endsWith(".*")) Recall(userInput.substring(0, userInput.indexOf(".*")), RECALL_MODE_RECURSIVE)
-          else Recall(userInput, RECALL_MODE_PLAIN)
-        }
+      case MarkPattern(hint, "None") => Remove(hint)
+      case MarkPattern(hint, content) => {
+        logger.debug("processing hint {},content {}", hint, content)
+        if (hint.contains(".")) {
+          logger.debug("hint contains '.'")
+          var indent = hint.substring(0, hint.lastIndexOf("."))
+          val newMark = indents match {
+            case head :: _ =>
+              indent = head + "." + indent
+              Mark(head + "." + hint, content)
+            case _ => Mark(hint, content)
+          }
+          markStack = newMark :: markStack
+          indents = indent :: indents
+          Continue(" " * indent.length())
+        } else if (indents.size > 0) {
+          markStack = Mark(indents.head + "." + hint, content) :: markStack
+          Continue(" " * indents.head.length())
+        } else Mark(hint, content)
+      }
+      case _ => { //should be some kind of recall
+        if (userInput.endsWith(".*")) Recall(userInput.substring(0, userInput.indexOf(".*")), RECALL_MODE_RECURSIVE)
+        else Recall(userInput, RECALL_MODE_PLAIN)
+      }
     }
   }
 
